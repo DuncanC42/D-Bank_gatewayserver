0ca98155c71ebcb630802ceabed12ccd72a3d428
shi2gami42 <cadoret.du@gmail.com>
2025-02-26 07:35:04 +0100

    Initial commit

---
+++ src/main/java/bzh/duncan/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,13 @@
+package bzh.duncan.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
